65a486d39daecefda594b08178688491b58de697
zubin aka zeuslawyer <zubin.pratap@gmail.com>
2018-09-25 15:02:38 +1000

    add partials. working

--- app.js
+++ app.js
@@ -7,22 +7,25 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var cafeRouter = require('./routes/cafes');
 var usersRouter = require('./routes/users');
+var submitRouter = require('./routes/submit-specials');
 
 var app = express();
 
-// view engine setup
+// view engine + dependencies setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//mount routes
 app.use('/', indexRouter);
 app.use('/cafe-login', cafeRouter);
 app.use('/users', usersRouter);
+app.use('/submit-specials', submitRouter);
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
@@ -38,6 +41,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error.ejs');
 });
+
 
 app.listen(3000, function(){
   console.log('Server started on 3000')
--- routes/submit-specials.js
+++ routes/submit-specials.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('cafe-login.ejs', { title: 'Express' });
+  res.render('submit-specials.ejs', { title: 'Express' });
 });
 
 module.exports = router;
