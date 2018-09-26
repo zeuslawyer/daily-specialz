6912eb9ad95040fa6bdb1a6107a60a4b6ce185c9
zubin aka zeuslawyer <zubin.pratap@gmail.com>
2018-09-26 14:59:00 +1000

    add json database. render index view with list implement. new specials form POST works

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var database = require('../database.json');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index.ejs', { title: 'Express' });
+  res.render('index.ejs', { specials: database.specials });
 });
 
 module.exports = router;
--- routes/submit-specials.js
+++ routes/submit-specials.js
@@ -1,15 +1,32 @@
 var express = require('express');
 var router = express.Router();
-var app
+var database= require('../database.json');
+var fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('submit-specials.ejs', { title: 'Express' });
 });
-router.post('/', function(req, res, next) {
-  console.log (req.body.specials);
+router.post('/', saveToDatabase, function(req, res, next) {
+
   res.render('test.ejs', {from: 'submit specials post route'})
 
 });
 
 module.exports = router;
+
+// =======================================
+// MIDDLEWARE
+// =======================================
+
+function saveToDatabase(req, res, next){
+  let specials = req.body.specials;
+  database.specials.push(specials);
+
+  let data = JSON.stringify(database);
+  fs.writeFileSync('database.json', data );
+  console.log(JSON.parse(data));
+  
+  next();
+}
+
