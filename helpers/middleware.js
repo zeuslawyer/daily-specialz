aa8d0f9250c965f16656be3a4b17fb44ee65ac79
zubin aka zeuslawyer <zubin.pratap@gmail.com>
2018-10-01 18:54:06 +1000

    WORKING. middleware now updates the new user data with coords and proper address. WORKING

--- helpers/middleware.js
+++ helpers/middleware.js
@@ -37,12 +37,15 @@ middleware.createNewFirebaseUser = (req, res, next) => {
 
 middleware.saveUserToDb = async function(req, res, next){
   let data = req.body
-  let coordinates = await geoCodeUser(data);
-  data.coordinates = coordinates
+  let updatedUserData = await updateUserCoordsAndAddress(data);
+  data = updatedUserData
 
   const USER_COLL = process.env.DB_USER_COLL || 'dev_env_users';
-
-  //get res.locals.userRecord from previous middleware
+  
+  /*
+  get res.locals.userRecord from previous middleware, 
+  and use the FIREBASE AUTH UserID as the key for the db entry 
+  */
   db.collection(USER_COLL).doc(res.locals.userRecord.uid).set(data)
     .then(writeTime => {
       // console.log(`\n===\nSaved new user with id...${res.locals.userRecord.uid}\n `)
@@ -83,21 +86,25 @@ middleware.isAuthenticated = function (req, res, next) {
 }
 
 
-async function geoCodeUser(userData){
+async function updateUserCoordsAndAddress(userData){
   let coordinates = []
   let address = userData.address;
-  // sample addres : https://geocoder.api.here.com/6.2/geocode.json?app_id=V6PIkh4NxmLvNdagihaa&app_code=QB2fWuLaD1jjddRZzCwa-Q&searchtext=36+batman+street fitzroy north
-  // const URL = here_api.geocodeURL+address
-  const URL = 'https://geocoder.api.here.com/6.2/geocode.json?app_id=V6PIkh4NxmLvNdagihaa&app_code=QB2fWuLaD1jjddRZzCwa-Q&searchtext=32%20cecil%20street%20fitzroy'
-  const resultData = await fetch(URL).then(d=>d.json())
+  const URL = here_api.geocodeURL+address
+  console.log('URL IS:   ', URL)
+  // const URL = 'https://geocoder.api.here.com/6.2/geocode.json?app_id=V6PIkh4NxmLvNdagihaa&app_code=QB2fWuLaD1jjddRZzCwa-Q&searchtext=32%20cecil%20street%20fitzroy'
+  const resultData = await fetch(URL)
+    .then(d=>d.json())
+    .catch(err => console.log('ERROR IN FETCHING GEOCODE\n ', err))
   // resultData.then(d=> console.log(resultData.Response.View[0].Result[0]))
   let lat = resultData.Response.View[0].Result[0].Location.DisplayPosition.Latitude
   let lon = resultData.Response.View[0].Result[0].Location.DisplayPosition.Longitude
+  updatedAddress = resultData.Response.View[0].Result[0].Location.Address.Label
   coordinates =[lat,lon]
   // console.log('\n=====\n', resultData.Response.View[0].Result[0].Location.DisplayPosition)
   console.log('\n=====\n', coordinates)
-
-  return coordinates
+  userData.address = updatedAddress;
+  userData.coordinates = coordinates;
+  return userData;
 }
 
 module.exports = middleware;
